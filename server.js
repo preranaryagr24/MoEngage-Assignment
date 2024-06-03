7713fb391adcdda7663d5bdc6639b001e20d4f66
preranaryagr24 <83573297+preranaryagr24@users.noreply.github.com>
2024-06-03 19:17:34 +0530

    back-end second commit

--- server.js
+++ server.js
@@ -1,56 +1,20 @@
-import express from "express";
-import dotenv from "dotenv";
-import mongoose from "mongoose";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import "express-async-errors";
-
-import errorHandlerMiddleware from "./middlewares/error-handler.js";
-import notFoundMiddleware from "./middlewares/not-found.js";
-import verifyJWT from "./middlewares/verifyJWT.js";
-import credentials from "./middlewares/credentials.js";
-import corsOptions from "../config/corsOptions.js";
-
-import userRoutes from "./routes/userRoutes.js";
-import { addReview, getReview } from "./controllers/reviewController.js";
-
-dotenv.config();
-
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
-
-//middleware
-
-app.use(credentials);
-
-// app.use(cors(corsOptions));
-
-app.use(express.json({ limit: "30mb", extended: true }));
-app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(cookieParser());
-
-const baseUrl = "/api/v1";
-
-app.use(`${baseUrl}/users`, userRoutes);
-
-app.get("/getReview/:brewerId", getReview);
-
-//test endpoint
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
-
-app.use(verifyJWT);
-// protected routes should come after this line
-app.post("/addReview", addReview);
-
-app.use(errorHandlerMiddleware);
-app.use(notFoundMiddleware);
-
 const PORT = process.env.PORT || 5000;
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() =>
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
-  )
-  .catch((error) => console.log(error.message));
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect('your_mongodb_connection_string', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+// Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/breweries', require('./routes/breweries'));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
